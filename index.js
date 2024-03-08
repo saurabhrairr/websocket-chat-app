da3887e675d58c698cb47501e1cb370859839b5b
saurabhnyuc <102672983+saurabhrairr@users.noreply.github.com>
2024-03-08 12:01:37 +0530

    all file added

---
+++ index.js
@@ -0,0 +1,27 @@
+const exprees = require("express");
+const app = exprees();
+const { Server } = require("socket.io");
+
+const path = require("path");
+const http = require("http");
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(exprees.static(path.resolve("./public")));
+server.listen(9000, () => {
+  console.log(`listening on ${9000}`);
+});
+
+app.get("/", (req, res) => {
+  return res.sendFile("/public/index.html");
+});
+
+
+
+
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+  });
+});
